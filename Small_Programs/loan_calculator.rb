2ade07ab4ec1fcbf78712d54340d04fe132a06c2
Jack Henson <jackhenson1@gmail.com>
2022-01-09 16:54:38 -0500

    Added fixes for Loan Calculator program

--- Small_Programs/loan_calculator.rb
+++ Small_Programs/loan_calculator.rb
@@ -9,6 +9,18 @@ def prompt(message)
   puts "=> #{message}"
 end
 
+def integer?(input)
+  input.to_i.to_s == input
+end
+
+def float?(input)
+  input.to_f.to_s == input || Float(input)
+end
+
+def number?(input)
+  integer?(input) || float?(input) rescue false
+end
+
 prompt(messages('welcome'))
 
 name = ''
@@ -24,16 +36,16 @@ end
 
 prompt("Hi, #{name}!")
 
-loop do  # main loop
+loop do # main loop
   total_loan_amount = ''
   loop do
     prompt(messages('amount'))
     total_loan_amount = gets.chomp
 
-    if total_loan_amount.empty? || total_loan_amount.to_f < 0
-      prompt(messages('valid number'))
-    else
+    if number?(total_loan_amount) && total_loan_amount.to_f >= 0 
       break
+    else
+      prompt(messages('valid number'))
     end
   end
 
@@ -42,10 +54,10 @@ loop do  # main loop
     prompt(messages('apr'))
     apr_percentage = gets.chomp
 
-    if apr_percentage.empty? || apr_percentage.to_f < 0
-      prompt(messages('valid number'))
-    else
+    if number?(apr_percentage) && apr_percentage.to_f >= 0 
       break
+    else
+      prompt(messages('valid number'))
     end
   end
 
@@ -54,25 +66,25 @@ loop do  # main loop
     prompt(messages('duration'))
     loan_duration_years = gets.chomp
 
-    if loan_duration_years.empty? || loan_duration_years.to_f < 0
-      prompt(messages('valid number'))
-    else
+    if number?(loan_duration_years) && loan_duration_years.to_f >= 0 
       break
+    else
+      prompt(messages('valid number'))
     end
   end
 
   loan_duration_months = loan_duration_years.to_f * 12
-  
   monthly_interest_percentage = apr_percentage.to_f / 12
+  monthly_int_decimal = monthly_interest_percentage.to_f / 100
+  monthly_payment = total_loan_amount.to_f *
+                    (monthly_int_decimal /
+                    (1 - (1 + monthly_int_decimal)**(-loan_duration_months)))
 
-  monthly_interest_decimal = monthly_interest_percentage.to_f / 100
-  
-  monthly_payment = total_loan_amount.to_f * (monthly_interest_decimal / (1 - (1 + monthly_interest_decimal)**(-loan_duration_months)))
-
-  if monthly_interest_percentage == 0  # for zero interest loans
-    prompt("Your monthly payment is $#{format("%.2f",(total_loan_amount.to_f / loan_duration_months))}.")
+  if monthly_interest_percentage == 0 # for zero interest loans
+    prompt("Your monthly payment is $#{format('%.2f', (total_loan_amount.to_f /
+                                                      loan_duration_months))}.")
   else
-    prompt("Monthly payment: $#{format("%.2f", monthly_payment)}")
+    prompt("Monthly payment: $#{format('%.2f', monthly_payment)}")
   end
 
   prompt(messages('again'))
