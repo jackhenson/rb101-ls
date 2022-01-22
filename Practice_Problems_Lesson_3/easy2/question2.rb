5d032313800feb9d441473e3dcd2174fb52c9a2e
Jack Henson <jackhenson1@gmail.com>
2022-01-22 10:19:59 -0500

    Added easy 2 problems

---
+++ Practice_Problems_Lesson_3/easy2/question2.rb
@@ -0,0 +1,13 @@
+munsters_description = "The Munsters are creepy in a good way."
+
+# "tHE mUNSTERS ARE CREEPY IN A GOOD WAY."
+p munsters_description.swapcase
+
+# "The munsters are creepy in a good way."
+p munsters_description.downcase.capitalize
+
+# "the munsters are creepy in a good way."
+p munsters_description.downcase
+
+# "THE MUNSTERS ARE CREEPY IN A GOOD WAY."
+p munsters_description.upcase
---
+++ Practice_Problems_Lesson_3/easy2/question8.rb
@@ -0,0 +1,6 @@
+advice = "Few things in life are as important as house training your pet dinosaur."
+advice.slice!('Few things in life are as important as ')
+p advice
+
+advice = "Few things in life are as important as house training your pet dinosaur."
+p advice['Few things in life are as important as ']
