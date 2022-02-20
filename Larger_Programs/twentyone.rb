23dbe90e3fc2ecad785efac5803a4c3092acd71d
Jack Henson <jackhenson1@gmail.com>
2022-02-20 16:38:18 -0500

    Updated twentyone program

--- Larger_Programs/twentyone.rb
+++ Larger_Programs/twentyone.rb
@@ -1,13 +1,48 @@
-def initialize_deck
-  new_deck = []
-  suit_set = ('1'..'9').to_a << 'jack' << 'queen' << 'king' << 'ace'
-  heart_suit = suit_set.map {|card| card << ' of hearts'}
-  diamond_suit = suit_set.map {|card| card << ' of diamonds'}
-  club_suit = suit_set.map {|card| card << ' of clubs'}
-  spade_suit = suit_set.map {|card| card << ' of spades'}
-  new_deck << (heart_suit + diamond_suit + club_suit + spade_suit)
-  new_deck.flatten
+# CONSTANTS
+WINNING_SCORE = 21
+
+# card_values = {'J': 10, 'Q': 10, 'K': 10, ''}
+
+player_hand = []
+dealer_hand = []
+
+def prompt(msg)
+  puts "=> #{msg}"
 end
 
-new_deck = initialize_deck
-p new_deck.size
+def initialize_deck
+  new_deck = []
+  card_values = ('1'..'9').to_a + ['J', 'Q', 'K', 'A']
+  card_suits = ['H', 'D', 'C', 'S']
+  card_values.each do |value|
+    card_suits.each do |suit|
+      new_deck << [suit, value]
+    end
+  end
+
+  new_deck
+end
+
+def pick_random_card(deck)
+  random_card = deck.sample
+  deck.delete(random_card)
+  random_card
+end
+
+def deal_card(deck, player_hand, dealer_hand)
+  player_hand << pick_random_card(deck)
+  dealer_hand << pick_random_card(deck)
+end
+
+def display_dealers_card(dealer_hand)
+  prompt "Dealer has: #{dealer_hand[0][1]} and unknown card"
+end
+
+def display_players_cards(player_hand)
+  prompt "You have: #{player_hand[0][1]} and #{player_hand[1][1]}"
+end
+
+deck = initialize_deck
+2.times { |deal| deal_card(deck, player_hand, dealer_hand) }
+display_dealers_card(dealer_hand)
+display_players_cards(player_hand)
