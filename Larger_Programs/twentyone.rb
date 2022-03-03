14a8e59dab43957c6b2631ad681a8fd2b52fc61c
Jack Henson <jackhenson1@gmail.com>
2022-03-02 21:24:18 -0500

    Updated Twentyone program

--- Larger_Programs/twentyone.rb
+++ Larger_Programs/twentyone.rb
@@ -1,4 +1,8 @@
+# Constants
+
 WINNING_SCORE = 21
+DEALER_HIT_MAX = 17
+SLEEP_DURATION = 1
 RULES = <<-MSG
   Goal: Get as close to 21 as possible, without going over.  
   You are the playing the dealer, you will both be dealt two cards.
@@ -10,6 +14,8 @@ RULES = <<-MSG
   Aces are worth 1 or 11, depending on your hand's total score.
 MSG
 
+# Auxillary methods
+
 def prompt(msg)
   puts "=> #{msg}"
 end
@@ -18,6 +24,28 @@ def continue
   prompt "Press Enter to continue"
   gets
 end
+
+def clear_screen
+  system 'clear'
+end
+
+def play_again?
+  loop do
+    prompt("Do you want to play again?")
+    prompt("Type 'Y' for yes or 'N' for no:")
+    answer = gets.chomp
+    clear_screen
+    if answer.downcase == 'y' || answer.downcase == 'yes'
+      return true
+    elsif answer.downcase == 'n' || answer.downcase == 'no'
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+end
+
+# Display
 
 def display_greeting
   prompt "Welcome to Twenty-One!"
@@ -28,7 +56,7 @@ def display_rules
   loop do
     prompt("Would you like to see the rules? (Type 'Y' for yes, 'N' for no.)")
     answer = gets.chomp
-    system 'clear'
+    clear_screen
     if answer.downcase == 'y' || answer.downcase == 'yes'
       puts RULES
       break
@@ -41,6 +69,186 @@ def display_rules
 
   continue
 end
+
+def display_cards(player_hand, dealer_hand)
+  display_dealers_cards(dealer_hand, hide_dealer_card: true)
+  display_players_cards(player_hand)
+end
+
+def display_dealers_cards(dealer_hand, hide_dealer_card: false)
+  display_hidden_dealer_cards(dealer_hand) if hide_dealer_card
+  display_all_dealer_cards(dealer_hand) if !hide_dealer_card
+end
+
+def display_hidden_dealer_cards(dealer_hand)
+  cards = 'Dealer has: '
+
+  dealer_hand.each do |card|
+    if dealer_hand.first == card
+      next
+    else
+      cards << expand_name(card[1]) + ' '
+    end
+  end
+
+  prompt cards + 'and hidden card'
+end
+
+def display_all_dealer_cards(dealer_hand)
+  cards = 'Dealer has: '
+
+  dealer_hand.each do |card|
+    cards << if dealer_hand.last == card
+               'and ' + expand_name(card[1])
+             elsif dealer_hand.size == 2
+               expand_name(card[1]) + ' '
+             else
+               expand_name(card[1]) + ', '
+             end
+  end
+
+  prompt cards
+end
+
+def display_players_cards(player_hand)
+  cards = 'You have: '
+
+  player_hand.each do |card|
+    cards << if player_hand.last == card
+               'and ' + expand_name(card[1])
+             elsif player_hand.size == 2
+               expand_name(card[1]) + ' '
+             else
+               expand_name(card[1]) + ', '
+             end
+  end
+
+  prompt cards
+end
+
+def display_player_total(scores)
+  puts ""
+  puts "=============="
+  prompt "Your current total: #{scores[:player]}"
+  puts "=============="
+  puts ""
+end
+
+def display_player_hit_results(player_hand, scores)
+  display_players_cards(player_hand)
+  display_player_total(scores)
+end
+
+def display_players_turn_results(scores)
+  busted?(scores) ? prompt('You busted!') : prompt("You chose to stay!")
+end
+
+def display_dealers_turn_results(scores)
+  busted?(scores) ? prompt('Dealer busted!') : prompt("Dealer chose to stay!")
+end
+
+def display_match_score(matches_won)
+  player = matches_won[:player]
+  dealer = matches_won[:dealer]
+  tie = matches_won[:tie]
+  puts "=============="
+  prompt "Current Match Total"
+  prompt "Player: #{player}. Dealer: #{dealer}. Ties: #{tie}"
+  puts "=============="
+  puts ""
+  continue
+  clear_screen
+end
+
+def display_total_score(scores)
+  puts ""
+  puts "=============="
+  prompt "Player total: #{scores[:player]}. Dealer total: #{scores[:dealer]}"
+  puts "=============="
+  puts ""
+end
+
+def display_winner(scores)
+  if determine_winner(scores) == 'Tie'
+    prompt "It's a tie!"
+  else
+    prompt "#{determine_winner(scores)} wins!"
+  end
+  continue
+  clear_screen
+end
+
+def display_tournament_winner(matches_won)
+  tournament_winner = determine_tournament_winner(matches_won)
+  prompt "#{tournament_winner} wins the Twenty-One tournament!"
+  puts ''
+end
+
+def display_goodbye
+  prompt("Thank you for playing! Goodbye.")
+end
+
+# Player action
+
+def player_choice
+  prompt "Do you want to hit or stay?"
+  prompt "Type 'H' to hit, or 'S' to stay:"
+  answer = gets.chomp
+  clear_screen
+  case answer.downcase
+  when 'h' || 'hit' then 'hit'
+  when 's' || 'stay' then 'stay'
+  else prompt "That's not a valid choice."
+  end
+end
+
+def players_turn(deck, player_hand, scores)
+  loop do
+    player_move = player_choice
+    if player_move == 'hit'
+      player_hits(deck, player_hand)
+      update_player_score(player_hand, scores)
+      display_player_hit_results(player_hand, scores)
+      break if scores[:player] == WINNING_SCORE
+      break if busted?(scores)
+    elsif player_move == 'stay'
+      break
+    end
+  end
+
+  display_players_turn_results(scores)
+  continue
+  clear_screen
+end
+
+def player_hits(deck, player_hand)
+  clear_screen
+  prompt "You hit!"
+  prompt "Dealing card..."
+  sleep SLEEP_DURATION
+  clear_screen
+  deal_card_player(deck, player_hand)
+end
+
+# Dealer actions
+
+def dealers_turn(deck, dealer_hand, scores)
+  until scores[:dealer] >= DEALER_HIT_MAX
+    prompt "Dealer chose to hit!"
+    prompt "Dealing card..."
+    sleep SLEEP_DURATION
+    puts ''
+    deal_card_dealer(deck, dealer_hand)
+    display_all_dealer_cards(dealer_hand)
+    update_dealer_score(dealer_hand, scores)
+  end
+
+  display_dealers_turn_results(scores)
+  continue
+  clear_screen
+end
+
+# Deck and dealing
 
 def initialize_deck
   new_deck = []
@@ -56,12 +264,12 @@ def initialize_deck
 end
 
 def first_deal(deck, player_hand, dealer_hand)
-  system 'clear'
+  clear_screen
   prompt "Dealing cards..."
-  sleep 1
-  system 'clear'
-  2.times { |deal| deal_card_player(deck, player_hand) }
-  2.times { |deal| deal_card_dealer(deck, dealer_hand) }
+  sleep SLEEP_DURATION
+  clear_screen
+  2.times { deal_card_player(deck, player_hand) }
+  2.times { deal_card_dealer(deck, dealer_hand) }
 end
 
 def pick_random_card(deck)
@@ -78,61 +286,6 @@ def deal_card_dealer(deck, dealer_hand)
   dealer_hand << pick_random_card(deck)
 end
 
-def display_dealers_cards(dealer_hand)
-  cards = []
-
-  dealer_hand.each do |card|
-    cards << expand_name(card[1])
-  end
-  
-  case dealer_hand.length
-  when 2 then prompt "Dealer has: #{cards[0]} and unknown card"
-  when 3 then prompt "Dealer has: #{cards[0]}, #{cards[2]}, and unknown card"
-  when 4 then prompt "Dealer has: #{cards[0]}, #{cards[2]}, #{cards[3]}, and unknown card"
-  when 5 then prompt "Dealer has: #{cards[0]}, #{cards[2]}, #{cards[3]}, #{cards[4]}, and unknown card"
-  end
-end
-
-def display_all_dealer_cards(dealer_hand)
-  system 'clear'
-  cards = []
-
-  dealer_hand.each do |card|
-    cards << expand_name(card[1])
-  end
-
-  hidden_card = cards[1]
-  case dealer_hand.length
-  when 2 then prompt "Dealer has: #{cards[0]} and #{hidden_card}"
-  when 3 then prompt "Dealer has: #{cards[0]}, #{cards[2]}, and #{hidden_card}"
-  when 4 then prompt "Dealer has: #{cards[0]}, #{cards[2]}, #{cards[3]}, and #{hidden_card}"
-  when 5 then prompt "Dealer has: #{cards[0]}, #{cards[2]}, #{cards[3]}, #{cards[4]}, and #{hidden_card}"
-  end
-end
-
-def display_players_cards(player_hand, scores)
-  cards = []
-
-  player_hand.each do |card|
-    cards << expand_name(card[1])
-  end
-
-  case player_hand.length
-  when 2 then prompt "You have: #{cards[0]} and #{cards[1]}"
-  when 3 then prompt "You have: #{cards[0]}, #{cards[1]}, and #{cards[2]}"
-  when 4 then prompt "You have: #{cards[0]}, #{cards[1]}, #{cards[2]}, and #{cards[3]}"
-  when 5 then prompt "You have: #{cards[0]}, #{cards[1]}, #{cards[2]}, #{cards[3]}, and #{cards[4]}"
-  end
-end
-
-def display_player_total(scores)
-  puts ""
-  puts "=============="
-  prompt "Your current total: #{scores[:player]}"
-  puts "=============="
-  puts ""
-end
-
 def expand_name(card)
   case card
   when 'J' then card = 'Jack'
@@ -144,6 +297,8 @@ def expand_name(card)
   card
 end
 
+# Scorekeeping
+
 def update_scores(player_hand, dealer_hand, scores)
   update_player_score(player_hand, scores)
   update_dealer_score(dealer_hand, scores)
@@ -151,21 +306,20 @@ end
 
 def update_player_score(player_hand, scores)
   values = player_hand.map { |card| card[1] }
-  
+
   player_score = 0
   values.each do |value|
-    if value == "A"
-      player_score += 11
-    elsif value.to_i == 0 # J, Q, K
-      player_score += 10
-    else
-      player_score += value.to_i
-    end
+    player_score += if value == "A"
+                      11
+                    elsif value.to_i == 0 # J, Q, K
+                      10
+                    else
+                      value.to_i
+                    end
   end
 
-  # correct for Aces
   values.select { |value| value == "A" }.count.times do
-    player_score -= 10 if player_score > 21
+    player_score -= 10 if player_score > WINNING_SCORE
   end
 
   scores[:player] = player_score
@@ -173,76 +327,23 @@ end
 
 def update_dealer_score(dealer_hand, scores)
   values = dealer_hand.map { |card| card[1] }
-  
+
   dealer_score = 0
   values.each do |value|
-    if value == "A"
-      dealer_score += 11
-    elsif value.to_i == 0 # J, Q, K
-      dealer_score += 10
-    else
-      dealer_score += value.to_i
-    end
+    dealer_score += if value == "A"
+                      11
+                    elsif value.to_i == 0 # J, Q, K
+                      10
+                    else
+                      value.to_i
+                    end
   end
 
-  # correct for Aces
   values.select { |value| value == "A" }.count.times do
-    dealer_score -= 10 if dealer_score > 21
+    dealer_score -= 10 if dealer_score > WINNING_SCORE
   end
 
   scores[:dealer] = dealer_score
-end
-
-def get_player_choice
-  prompt "Do you want to hit or stay?"
-  prompt "Type 'H' to hit, or 'S' to stay:"
-  answer = gets.chomp
-    system 'clear'
-    case answer.downcase
-    when 'h' || 'hit' then return 'hit'
-    when 's' || 'stay' then return 'stay'
-    else prompt "That's not a valid choice."
-    end
-end
-
-def players_turn(deck, player_hand, scores)
-  loop do
-    player_choice = get_player_choice
-    if player_choice == 'hit'
-      system 'clear'
-      prompt "You hit!"
-      prompt "Dealing card..."
-      sleep 1
-      system 'clear'
-      deal_card_player(deck, player_hand)
-      update_player_score(player_hand, scores)
-      display_players_cards(player_hand, scores)
-      display_player_total(scores)
-      break if busted?(scores)
-    elsif player_choice == 'stay'
-      break
-    end
-  end
-
-  busted?(scores) ? prompt('You busted!') : prompt("You chose to stay!")
-  continue
-  system 'clear'
-end
-
-def dealers_turn(deck, dealer_hand, scores)
-  until scores[:dealer] >= 17
-    prompt "Dealer chose to hit!"
-    prompt "Dealing card..."
-    sleep 1
-    puts ''
-    deal_card_dealer(deck, dealer_hand)
-    display_all_dealer_cards(dealer_hand)
-    update_dealer_score(dealer_hand, scores)
-  end
-
-  busted?(scores) ? prompt('Dealer busted!') : prompt("Dealer chose to stay!")
-  continue
-  system 'clear'
 end
 
 def busted?(scores)
@@ -250,9 +351,9 @@ def busted?(scores)
 end
 
 def determine_winner(scores)
-  if scores[:player] > 21
+  if scores[:player] > WINNING_SCORE
     'Dealer'
-  elsif scores[:dealer] > 21 
+  elsif scores[:dealer] > WINNING_SCORE
     'Player'
   elsif scores[:player] > scores[:dealer]
     'Player'
@@ -271,54 +372,6 @@ def update_match_score(scores, matches_won)
   end
 end
 
-def display_match_score(matches_won)
-  puts "=============="
-  prompt "Current Match Total"
-  prompt "Player: #{matches_won[:player]}. Dealer: #{matches_won[:dealer]}. Ties: #{matches_won[:tie]}"
-  puts "=============="
-  puts ""
-  continue
-  system 'clear'
-end
-
-def display_total_score(scores)
-  puts ""
-  puts "=============="
-  prompt "Player total: #{scores[:player]}. Dealer total: #{scores[:dealer]}"
-  puts "=============="
-  puts ""
-end
-
-def display_winner(scores)
-  if determine_winner(scores) == 'Tie'
-    prompt "It's a tie!"
-  else
-    prompt "#{determine_winner(scores)} wins!"
-  end
-  continue
-  system 'clear'
-end
-
-def play_again?
-  loop do
-    prompt("Do you want to play again?")
-    prompt("Type 'Y' for yes or 'N' for no:")
-    answer = gets.chomp
-    system 'clear'
-    if answer.downcase == 'y' || answer.downcase == 'yes'
-      return true
-    elsif answer.downcase == 'n' || answer.downcase == 'no'
-      break
-    else
-      prompt("That's not a valid choice.")
-    end
-  end
-end
-
-def tournament_over?(matches_won)
-  matches_won[:player] == 5 || matches_won[:dealer] == 5
-end
-
 def determine_tournament_winner(matches_won)
   if matches_won[:player] == 5
     'Player'
@@ -327,13 +380,8 @@ def determine_tournament_winner(matches_won)
   end
 end
 
-def display_tournament_winner(matches_won)
-  prompt "#{determine_tournament_winner(matches_won)} wins the Twenty-One tournament!"
-  puts ''
-end
-
-def display_goodbye
-  prompt("Thank you for playing! Goodbye.")
+def tournament_over?(matches_won)
+  matches_won[:player] == 5 || matches_won[:dealer] == 5
 end
 
 # Begin game
@@ -342,23 +390,21 @@ display_greeting
 display_rules
 
 loop do
-  matches_won = {player: 0, dealer: 0, tie: 0}
+  matches_won = { player: 0, dealer: 0, tie: 0 }
 
   loop do
     deck = initialize_deck
     player_hand = []
     dealer_hand = []
-    scores = {player: 0, dealer: 0}
+    scores = { player: 0, dealer: 0 }
 
     first_deal(deck, player_hand, dealer_hand)
     update_scores(player_hand, dealer_hand, scores)
-    display_dealers_cards(dealer_hand)
-    display_players_cards(player_hand, scores)
+    display_cards(player_hand, dealer_hand)
     display_player_total(scores)
     players_turn(deck, player_hand, scores)
     dealers_turn(deck, dealer_hand, scores) unless busted?(scores)
-    display_total_score(scores)
-    update_scores(player_hand, dealer_hand, scores)
+    display_total_score(scores) unless busted?(scores)
     display_winner(scores)
     update_match_score(scores, matches_won)
     display_match_score(matches_won)
