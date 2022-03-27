254355fac33524eff8b5cb1afedfe3124c0e6c05
Jack H <jackhensonmedia@gmail.com>
2022-03-27 15:45:34 -0400

    Added Debugging exercises

---
+++ Exercises/Debug/exercise_2.rb
@@ -0,0 +1,20 @@
+# String#upcase! is a destructive method, so why 
+# does this code print HEY you instead of HEY YOU? 
+# Modify the code so that it produces 
+# the expected output.
+
+# def shout_out_to(name)
+#   name.chars.each { |c| c.upcase! }
+
+#   puts 'HEY ' + name
+# end
+
+# shout_out_to('you') # expected: 'HEY YOU'
+
+def shout_out_to(name)
+  name.upcase!
+
+  puts 'HEY ' + name
+end
+
+shout_out_to('you') # expected: 'HEY YOU'
